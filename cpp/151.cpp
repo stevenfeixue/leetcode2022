2de7d2a061f7c78460e1e3aeb8ea469113cd3e52
Fei Xue <xuefei2062@outlook.com>
2022-06-26 15:27:12 +0800

    No.151

--- cpp/151.cpp
+++ cpp/151.cpp
@@ -16,11 +16,39 @@ public:
         return size - space_count;
     }
 
+    int removeExtraSpaces(string &s, int size) {
+        int i; /*fast index*/
+        int j; /*slow index*/
+        for (i=0, j=0; i<size; ++i) {
+            if (i>0 && s[i-1] == s[i] && s[i] == ' ') {
+                continue;
+            } else
+                s[j++] = s[i];
+        }
+        s.resize(j);
+        return j;
+    }
+
     string reverseWords(string s) {
         int size = s.size();
         size = trim(s, size);
         reverse(s.begin(), s.end());
-        trim(s, size); 
+        size = trim(s, size);
+        size = removeExtraSpaces(s, size);
+
+        auto left = s.begin();
+        int n;
+        while (left != s.end()) {
+            n = 0;
+            while ((left+n) != s.end() && *(left+n) != ' ') n++;
+            
+            reverse(left, left+n);
+
+            left = left + n;
+            if (left == s.end()) break;
+            else
+                left++;
+        }
         
         return s;
     }
@@ -29,7 +57,7 @@ public:
 
 int main() {
     Solution sl;
-    string s{"    the  blue  sky       "};
+    string s{"    abc  defgh   ijk       "};
     cout << sl.reverseWords(s) << endl;
     return 0;
 }
